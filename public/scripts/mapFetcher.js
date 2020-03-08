f6a11be2596fef8baa4dd2225cc7d263e0de042f
maridoh13 <maridoh@gmail.com>
2020-03-08 01:38:40 +0000

    loop in function initMap

--- public/scripts/mapFetcher.js
+++ public/scripts/mapFetcher.js
@@ -1,36 +1,50 @@
 // Map function
 
-// The locations
-var vancity = {lat: 49.2945789, lng: -123.1182459};
-var gyu = {lat: 49.280058, lng: -123.1252331};
-var sushi = {lat: 49.2794237, lng: -123.1078451};
-var contentString = "hello world";
-var string2 = "howdy do";
-
-
 function initMap() {
+  const point1 = {
+    name: "Gyu",
+    lat: 49.280058,
+    long: -123.1252331,
+    content: "Best Japanese BBQ",
+    infoWin: "infoG"
+  };
+
+  const point2 = {
+    name: "SushiDen",
+    lat: 49.2794237,
+    long: -123.1078451,
+    content: "Cheapest Sushi!",
+    infoWin: "infoS"
+  }
+
+  let places = [point1, point2];
+
+
   // The map
+  var vancity = { lat: 49.2945789, lng: -123.1182459 };
   var map = new google.maps.Map(
-    document.getElementById('map'), {zoom: 12, center: vancity});
+    document.getElementById('map'), { zoom: 12, center: vancity }
+  );
 
-  var marker1 = new google.maps.Marker({position: gyu, map: map, animation: google.maps.Animation.DROP});
-  var marker2 = new google.maps.Marker({position: sushi, map: map, animation: google.maps.Animation.DROP});
+  for (let i = 0; i < places.length; i++) {
+    let nameMarker = places[i].name;
+    let infoWin = places[i].infoWin;
 
+    nameMarker = new google.maps.Marker({
+      position: { lat: places[i].lat, lng: places[i].long },
+      map: map,
+      animation: google.maps.Animation.DROP
+    });
 
-  var infowindow1 = new google.maps.InfoWindow({
-    content: contentString
-  });
+    infoWin = new google.maps.InfoWindow({
+      content: places[i].content
+    });
 
-  var infowindow2 = new google.maps.InfoWindow({ content: string2})
+    nameMarker.addListener('click', function () {
+      infoWin.open(map, nameMarker);
+    });
 
-  marker1.addListener('click', function() {
-    infowindow1.open(map, marker1);
-  });
-
-  marker2.addListener('click', function() {
-    infowindow2.open(map, marker2)
-  });
-
+  }
 
 };
 

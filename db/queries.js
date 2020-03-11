15d11bf4446ba1b370b26858782c763710f375f8
Anna Gordon <annetta@email.com>
2020-03-11 16:05:21 +0000

    fixes after db changes

--- db/queries.js
+++ db/queries.js
@@ -55,7 +55,6 @@ const getUserByEmail = (email) => {
 const getMapByType = (type) => {
   return db.query(`SELECT * FROM maps WHERE type=$1;`, [type])
   .then(data => {
-    console.log(data.rows)
     return data.rows;
   })
   .catch(err => {

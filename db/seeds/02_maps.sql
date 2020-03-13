b6b540e2960293aa677d811eeeb191f2ad629ded
maridoh13 <maridoh@gmail.com>
2020-03-13 17:42:43 +0000

    added favs to parks / small fixes

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,9 +1,10 @@
 INSERT INTO maps (user_id, name, type) VALUES (1, 'Best pickles!', 'restaurant');
-INSERT INTO maps (user_id, name, type) VALUES (2, 'Best parks', 'park');
+INSERT INTO maps (user_id, name, type) VALUES (2, 'Parks to chill in Vancouver', 'park');
 INSERT INTO maps (user_id, name, type) VALUES (2, 'Best Japanese places in town', 'restaurant');
 INSERT INTO maps (user_id, name, type) VALUES (4, 'Parks in NYC', 'park');
 INSERT INTO maps (user_id, name, type) VALUES (1, 'Fav restaurants in Kits', 'restaurant');
-INSERT INTO maps (user_id, name, type) VALUES (1, 'Theaters in Van', 'places');
-INSERT INTO maps (user_id, name, type) VALUES (4, 'Places to go in Van', 'places');
+INSERT INTO maps (user_id, name, type) VALUES (1, 'Places to take pictures', 'park');
+INSERT INTO maps (user_id, name, type) VALUES (4, 'Late nite joints', 'restaurant');
+INSERT INTO maps (user_id, name, type) VALUES (4, 'Dog parks', 'park');
 
 
